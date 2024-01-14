57c9ff71717132c0345bb78a8ccc7240b8b439eb
Adelchi Asta <adelchiasta@localhost.localdomain>
2024-01-14 12:02:09 +0100

    uncommenting drift_detection_script

--- drift_detection_script.py
+++ drift_detection_script.py
@@ -9,7 +9,7 @@ from sklearn.metrics import roc_curve, auc, brier_score_loss
 # Global variables 
 FEATURES_COVARIATE = ["Temperature",  "Humidity", "Light", "CO2", "HumidityRatio"]
 TARGET = "Occupancy"
-MODEL_DRIFT_TOL=0.00002 # i.e. two auc pts
+MODEL_DRIFT_TOL=0.02 # i.e. two auc pts
 
 
 def import_model():
@@ -86,12 +86,12 @@ if __name__ == '__main__':
     # check if there was a drift 
     if (answer_drift_covariate=='Yes!') | (answer_drift_predictions=='Yes'):
         # check if model performance is drifting
-        # if performance_drift>=MODEL_DRIFT_TOL:
+        if performance_drift>=MODEL_DRIFT_TOL:
             # Save the drift detection result to a file
-        result_retraining = "retraining_result.txt"
-        with open(result_retraining, "w") as result_file:
-            result_file.write('Yes!')
-        # scratch the training data with the updated data, so that a new model can be retrained
-        df_merged.to_csv("datatraining.txt")
-        print("data_training.txt was modified")
+            result_retraining = "drift_detection_result.txt"
+            with open(result_retraining, "w") as result_file:
+                result_file.write('Yes!')
+            # scratch the training data with the updated data, so that a new model can be retrained
+            df_merged.to_csv("datatraining.txt")
+            print("data_training.txt was modified")
     print('ALL good mate')
